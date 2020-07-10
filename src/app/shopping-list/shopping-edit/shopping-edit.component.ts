bc0a0189397da64300159620094f0c31ee87dbcb
Yaswanth Bonda <yaswanthbonda@gmail.com>
2020-07-10 19:50:16 -0400

    updated the course project with template driven Forms on shopping list and edit components

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,32 +1,71 @@
 import {
   Component,
   OnInit,
-  ElementRef,
+  OnDestroy,
   ViewChild
 } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  editMode = false;
+  editingItemIndex : number;
+  editingItem: Ingredient;
+   @ViewChild('myForm', { static: false }) slForm: NgForm;
+  // @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  // @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.subscription = this.slService.startedEditing.subscribe( (index: number)=> {
+        this.editMode = true; 
+        // Because we only come inside of this subscription if we are editing something, 
+        // so change the default value from false to true so that we will know the difference between editing and new
+        this.editingItemIndex = index;
+        this.editingItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name : this.editingItem.name,
+          amount : this.editingItem.amount
+        });
+      }
+    );
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
-    this.slService.addIngredient(newIngredient);
+  onSubmit(form : NgForm) {
+    // const ingName = this.nameInputRef.nativeElement.value;
+    // const ingAmount = this.amountInputRef.nativeElement.value;
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if(this.editMode){
+      this.slService.updateIngredient(this.editingItemIndex, newIngredient);
+    }else this.slService.addIngredient(newIngredient);
+    this.editMode = false;
+    form.reset();
+  }
+
+  onClear(){
+    // console.log("i am clicking clear button");
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete(){
+    this.slService.deleteIngredient(this.editingItemIndex);
+    this.onClear();
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -25,6 +25,9 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
       );
   }
 
+  onEditItem(index: number){
+    this.slService.startedEditing.next(index);
+  }
   ngOnDestroy(): void{
     this.igChangedSub.unsubscribe();
   }
